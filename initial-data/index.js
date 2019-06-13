6b8146662711f3d1bb85b7fa2d179a47d2005f6d
Jason Liao <jtliao@fb.com>
2019-06-13 09:49:06 -0700

    Update index.js

--- initial-data/index.js
+++ initial-data/index.js
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ */
+
 const fs = require('fs');
 const fetch = require('node-fetch');
 const {validateSamples} = require('../shared');
